c2e11576d51b40d0cd202a529ad9e23784c7a788
Sinfinite01 <54284225+djlouie@users.noreply.github.com>
2025-04-06 13:14:09 -0700

    Adding Code Journal 1 Prompt 1

---
+++ cj1-prompt1.py
@@ -0,0 +1,19 @@
+def PrintFullName():
+    print("Dylan Joshua Louie")
+
+def PrintFavoriteMovieDiscussion():
+    print("My favorite movie is the romance \"Before Sunrise\" because of its unconvential way of displaying love and emotion.\n" \
+    "It does this by having the movie and its drama solely surround conversations between Jesse and Celine, thus making it feel intimate, relatable, and special.")
+
+def PrintFavoriteFood():
+    print("My favorite food is a Bacon Cheeseburger w/ a Fried Egg")
+
+# define main
+def main():
+    PrintFullName()
+    PrintFavoriteMovieDiscussion()
+    PrintFavoriteFood()
+
+# tells the program to run this if name is main which is when the file runs directly
+if __name__ == "__main__":
+    main()
