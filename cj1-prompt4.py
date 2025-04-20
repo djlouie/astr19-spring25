661131411ab830fa4b742c5799ec8508e86d422a
Sinfinite01 <54284225+djlouie@users.noreply.github.com>
2025-04-20 09:05:41 -0700

    Fixes printout of the hamster class's eye class variable. Also switches the init function to use default vbariables.

--- cj1-prompt4.py
+++ cj1-prompt4.py
@@ -1,15 +1,22 @@
 class Hamster:
-    def __init__(self):
-        self.length_of_arms = 0.0
-        self.length_of_legs = 11.0  # mm
-        self.num_eyes = 2
-        self.has_tail = True
-        self.is_furry = True
+    def __init__(self,
+                length_of_arms = 0.0,
+                length_of_legs = 11.0,
+                num_eyes = 2,
+                has_tail = True,
+                is_furry = True,
+                ):
+        
+        self.length_of_arms = length_of_arms
+        self.length_of_legs = length_of_legs  # mm
+        self.num_eyes = num_eyes
+        self.has_tail = has_tail
+        self.is_furry = is_furry
 
     def print_characteristics(self):
         print(f"The length of this hamster's arms is {self.length_of_arms} mm.")
         print(f"The length of this hamster's legs is {self.length_of_legs} mm.")
-        print(f"This hamster has {self.length_of_legs} eyes.")
+        print(f"This hamster has {self.num_eyes} eyes.")
 
         if self.has_tail:
             print('This hamster has a tail.')
